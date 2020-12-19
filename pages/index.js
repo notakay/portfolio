a05053f45ea041a320a5525d1c7fd009a8ec12ea
akay <aungkhantko@protonmail.com>
2020-12-19 23:56:00 +0630

    Added index page

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,35 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Typed from "react-typed";
+import styles from "../styles/Home.module.css";
+
+function Link(props) {
+  return (
+    <a className={styles.link} href={props.url} target="_blank">
+      {props.text}
+    </a>
+  );
+}
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <div className={styles.intro}>
+        <h1 className={styles.header1}>Hi! I'm Akay.</h1>
+        <h2 className={styles.header2}>
+          I'm a&nbsp;
+          <Typed
+            strings={["Student", "Coder", "Hacker"]}
+            typeSpeed={50}
+            backSpeed={60}
+            loop
+          ></Typed>
+        </h2>
+      </div>
+      <div>
+        <Link url="#" text="Blog" />
+        <Link url="https://www.github.com/notakay" text="GitHub" />
+        <Link url="https://www.twitter.com/notakay_" text="Twitter" />
+        <Link url="https://www.linkedin.com/in/aungkhantko" text="LinkedIn" />
+      </div>
     </div>
-  )
+  );
 }
